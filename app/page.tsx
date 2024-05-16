3650299f7d15b9ba818c7a383be6f78b145dcae2
Alef Ramos <victorramos123@outlook.com>
2024-05-16 18:26:15 -0300

    refactor: move link inside button

--- app/page.tsx
+++ app/page.tsx
@@ -46,15 +46,16 @@ const Home = async () => {
       <div className="space-y-4 pt-6">
         <div className="flex items-center justify-between px-5">
           <h2 className="font-semibold">Pedidos Recomendados</h2>
-          <Link href="/products/recommended">
-            <Button
-              variant="ghost"
-              className="h-fit p-0 font-medium text-primary hover:bg-transparent hover:text-primary"
-            >
+          <Button
+            variant="ghost"
+            className="h-fit p-0 font-medium text-primary hover:bg-transparent hover:text-primary"
+            asChild
+          >
+            <Link href="/products/recommended">
               Ver todos
               <ChevronRightIcon size={16} />
-            </Button>
-          </Link>
+            </Link>
+          </Button>
         </div>
         <ProductList products={products} />
       </div>
@@ -69,15 +70,16 @@ const Home = async () => {
       <div className="space-y-4 pb-6 pt-6">
         <div className="flex items-center justify-between px-5">
           <h2 className="font-semibold">Restaurantes Recomendados</h2>
-          <Link href="/restaurants/recommended">
-            <Button
-              variant="ghost"
-              className="h-fit p-0 font-medium text-primary hover:bg-transparent hover:text-primary"
-            >
+          <Button
+            variant="ghost"
+            className="h-fit p-0 font-medium text-primary hover:bg-transparent hover:text-primary"
+            asChild
+          >
+            <Link href="/restaurants/recommended">
               Ver todos
               <ChevronRightIcon size={16} />
-            </Button>
-          </Link>
+            </Link>
+          </Button>
         </div>
         <RestaurantList />
       </div>
