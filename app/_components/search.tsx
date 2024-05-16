bdbdcc64b7ca34ae9d8de6a3545efc4718f6b44a
Alef Ramos <victorramos123@outlook.com>
2024-05-16 19:19:42 -0300

    ensure restaurants can be searched

--- app/_components/search.tsx
+++ app/_components/search.tsx
@@ -1,18 +1,41 @@
+"use client";
+
 import { SearchIcon } from "lucide-react";
 import { Button } from "./ui/button";
 import { Input } from "./ui/input";
+import { FormEventHandler, useState } from "react";
+import { useRouter } from "next/navigation";
 
 const Search = () => {
+  const [search, setSearch] = useState("");
+  const router = useRouter();
+
+  const handleSearch = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSearch(e.target.value);
+  };
+
+  const handleSearchSubmit: FormEventHandler<HTMLFormElement> = (e) => {
+    e.preventDefault();
+
+    if (!search) {
+      return;
+    }
+
+    router.push(`/restaurants?search=${search}`);
+  };
+
   return (
-    <div className="flex gap-1">
+    <form className="flex gap-1" onSubmit={handleSearchSubmit}>
       <Input
         placeholder="Buscar restaurantes..."
         className="border-none text-lg font-medium"
+        onChange={handleSearch}
+        value={search}
       />
-      <Button size="icon">
+      <Button size="icon" type="submit">
         <SearchIcon size={20} />
       </Button>
-    </div>
+    </form>
   );
 };
 
